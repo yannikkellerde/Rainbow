4ae2f48f943d142e50fc368fbce3a11305746338
yannikkellerde <yannik@kelnet.de>
2023-01-21 11:03:03 +0000

    better logging

--- train.py
+++ train.py
@@ -41,7 +41,7 @@ if __name__ == '__main__':
 
     # set up logging & model checkpoints
 
-    wandb.init(project='rainbow_hex', save_code=True, config=dict(**wandb_log_config, log_version=100),
+    wandb.init(project='rainbow_hex', save_code=True, config=dict(**wandb_log_config, log_version=100, growth_schedule=growth_schedule),
                mode=('online' if args.use_wandb else 'offline'), anonymous='allow', tags=args.wandb_tag.split(",") if args.wandb_tag else [])
     if args.use_wandb:
         save_dir = Path("checkpoints") / wandb.run.name
@@ -120,7 +120,7 @@ if __name__ == '__main__':
             "returns":deque(maxlen=100),
             "lengths":deque(maxlen=100)
             }
-    growth_schedule = {6:(6,3,3600*2,1),7:(6,3,3600*2,1),8:(6,3,3600*2,1),9:(6,3,3600*3,1),10:(8,5,3600*3,1),11:(8,5,3600*1000,0)}
+    growth_schedule = {6:(6,3,3600*7,1),7:(6,3,3600*10,1),8:(6,3,3600*13,1),9:(6,3,3600*16,1),10:(8,5,3600*20,1),11:(8,5,3600*1000,0)}
 
     returns_all = []
     q_values_all = []
@@ -133,7 +133,7 @@ if __name__ == '__main__':
     checkpoint_frames = 300_000
     eval_frames = 80_000
     log_frames = 2_000
-    jumping_extra = int(3600*2)
+    jumping_extra = int(3600*4)
 
     if args.testing_mode:
         checkpoint_frames = 20_000
